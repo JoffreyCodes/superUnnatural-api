62bec3f3a64281e7bbbc881a98c559510a0771c9
inocejoff <inocejoff@gmail.com>
2022-12-01 10:24:19 -0800

    updated data processor

--- MySql/tableProcessor.py
+++ MySql/tableProcessor.py
@@ -2,19 +2,158 @@ import csv
 '''
 Processes airtable csv with focus on the Workouts view. 
 Location table is handled separately.
-The read-in csv expects the following columns:
+The INPUT_WORKOUTS_FILE csv expects the following columns:
     Name                    Description                 Date
     Name (from Intensity)   SN ID (from Intensity)      Chew Rating
     Length                  Name (from Coach)           SN ID (from Coach)
     Name (from Genre)       SN ID (from Genre)          Song Name (from Songs)
     Artist (from Songs)     Length (from Songs)         SN ID (from Songs)
     Spotify Link            SN ID                       Triangles
-    Max Targets             Screenshot (from Locations) Name (from Locations)
-    SN ID (from Locations)  Near the Ground (from Locations)
-    Max Score               Points Per Minute           Targets Per Minute
-    Workout Type            Max Dodges                  Max Knee Strikes
+    Max Targets             Max Score                   Points Per Minute
+    Targets Per Minute      Workout Type                Max Dodges
+    Max Knee Strikes        SN ID (from Locations)
 '''
-data = {
+
+INPUT_WORKOUTS_FILE = './Datasets/WORKOUTS_FLATTENED_11_29_2022.csv'
+INPUT_LOCATIONS_FILE = './Datasets/LOCATIONS_FLATTENED_11_29_2022.csv'
+PATH = "./Datasets/ProcessedFiles/"
+
+def main():
+    runProcessor()
+    for table in createInstrxn.keys():
+        createCSV(createInstrxn[table]['filename'], createInstrxn[table]['headers'], createInstrxn[table]['items'])
+
+def runLocationsProcessor():
+    with open(INPUT_LOCATIONS_FILE, mode='r', encoding="utf-8-sig") as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        next(reader, None)
+        for row in reader:
+            locationEntry = {}
+            for i in range(len(row)):
+                colNames = createInstrxn['locations']['headers']
+                locationEntry[colNames[i]] = row[i]
+            createInstrxn['locations']['items'].append(locationEntry)
+
+def getHeaders():
+    headers = {}
+    with open(INPUT_WORKOUTS_FILE, mode='r', encoding="utf-8-sig") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            for col in range(len(row)):
+                colName = row[col]
+                headers[colName] = col
+            break
+    return headers
+
+def get(val):
+    headers = getHeaders()
+    return headers.get(val)
+
+
+def runProcessor():
+    # process locations using locations data file
+    runLocationsProcessor()
+    with open(INPUT_WORKOUTS_FILE, mode='r', encoding="utf-8-sig") as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        # skip header row
+        next(reader, None)
+        for row in reader:
+            processWorkoutType(row)
+            processTable(row,'songs')
+            processTable(row,'coaches')
+            processTable(row,'genres')
+            processTable(row,'intensities')
+            handleGenreList(row, 'genrelist')
+            handleSongMapLocationList(row, 'songmaps')
+
+def handleGenreList(row, table):
+    WorkoutId = row[get('SN ID')]
+    GenreIdListStr = row[get('SN ID (from Genre)')]
+    if(GenreIdListStr == ''):
+        createGenreList(WorkoutId, table, '')
+    GenreIdList = [id.strip() for id in GenreIdListStr.split(',')]
+    for i in range(len(GenreIdList)):
+        createGenreList(WorkoutId, table, GenreIdList[i])
+
+
+def createGenreList(WorkoutId, table, GenreId):
+    genreList = createInstrxn[table]['items']
+    if(GenreId):
+        genreList.append({'SN ID':WorkoutId, 'SN ID (from Genre)' : GenreId})
+    else:
+        genreList.append({'SN ID':WorkoutId})
+
+
+def handleSongMapLocationList(row, table):
+    WorkoutId = row[get('SN ID')]
+    SongIdListStr = row[get('SN ID (from Songs)')]
+    LocationIdListStr = row[get('SN ID (from Locations)')]
+    songMapList = createInstrxn[table]['items']
+    if SongIdListStr == '' or LocationIdListStr == '':
+        return
+    songIdList = [id.strip() for id in SongIdListStr.split(',')]
+    locationIdList = [id.strip() for id in LocationIdListStr.split(',')]
+    # determine minimun len of each row (some numSongs != numLocs)
+    lenSongIdList = len(songIdList)
+    lenLocationIdList = len(locationIdList)
+    minLen = min(lenSongIdList, lenLocationIdList)
+    # flatten each row and append to new list
+    for i in range(minLen):
+        songMapList.append({
+            'SN ID':WorkoutId, 
+            'SN ID (from Songs)': songIdList[i], 
+            'SN ID (from Locations)':locationIdList[i]
+        })
+
+def processTable(row, table):
+    # if no elems, skip
+    tablePk = createInstrxn[table]['primary_key']
+    colTablePk = row[get(tablePk)]    
+    if colTablePk == '' : return
+    
+    # handle multiple elems in a row
+    tablePkList = [int(pk) for pk in colTablePk.split(',')]
+    numElems = len(tablePkList)
+    for i in range(numElems):
+        # skip if already encountered (from prev rows)
+        pk = tablePkList[i]
+        pkSet = createInstrxn[table]['pk_set']
+        if pk in pkSet: continue
+        
+        # create entry, add elems to list (via headers), add pk to set  
+        tableElem = {}
+        headers = createInstrxn[table]['headers']
+        for header in headers:
+            tableElem[header] = [item.strip() for item in row[get(header)].split(',')][i]
+        tableList = createInstrxn[table]['items']
+        tableList.append(tableElem) 
+        pkSet.add(pk)
+
+def processWorkoutType(row):
+    processRow(row, createInstrxn['workouts']['items'], createInstrxn['workouts']['headers'])
+    workoutType = row[get('Workout Type')]
+    if(workoutType == 'classic'):
+        processRow(row, createInstrxn['classic_workouts']['items'], createInstrxn['classic_workouts']['headers'])
+    if(workoutType == 'boxing'):
+        processRow(row, createInstrxn['boxing_workouts']['items'], createInstrxn['boxing_workouts']['headers'])
+    if(workoutType == 'meditation'):
+        processRow(row, createInstrxn['meditation_workouts']['items'], createInstrxn['meditation_workouts']['headers'])
+    if(workoutType == 'stretch'):
+        processRow(row, createInstrxn['stretch_workouts']['items'], createInstrxn['stretch_workouts']['headers'])
+
+def processRow(row, list, headers):
+    createInstrxn = {}
+    for header in headers:
+        createInstrxn[header] = row[get(header)] if row[get(header)] != '' else 0
+    list.append(createInstrxn)
+
+def createCSV(filename, headers, items):
+    with open(PATH + filename, "w", newline="", encoding="utf-8-sig") as f:
+        writer = csv.DictWriter(f, headers)
+        writer.writeheader()
+        writer.writerows(items)
+
+createInstrxn = {
     'workouts': {
         'items' : [],
         'headers' : [
@@ -90,84 +229,26 @@ data = {
         'primary_key': 'SN ID (from Intensity)',
         'filename' : 'INTENSITIES.csv'
     },
+    'locations': {
+        'items' : [],
+        'headers' : [
+            'Name', 'Screenshot',
+            'SN ID', 'Near the Ground'], 
+        'filename' : 'LOCATIONS.csv'
+    },
+    'songmaps': {
+        'items' : [],
+        'headers' : [
+            'SN ID', 'SN ID (from Songs)',
+            'SN ID (from Locations)'], 
+        'filename' : 'SONGMAPSLIST.csv'
+    },
+    'genrelist': {
+        'items' : [],
+        'headers' : [
+            'SN ID', 'SN ID (from Genre)'], 
+        'filename' : 'GENRELIST.csv'
+    }
 }
 
-PATH = "./Datasets/ProcessedFiles/"
-
-def getHeaders():
-    headers = {}
-    with open('./Datasets/WORKOUTS_FLATTENED_11_29_2022.csv', mode='r', encoding="utf-8-sig") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            for col in range(len(row)):
-                colName = row[col]
-                headers[colName] = col
-            break
-    return headers
-
-def get(val):
-    return headers.get(val)
-
-def runProcessor():
-    with open('./Datasets/WORKOUTS_FLATTENED_11_29_2022.csv', mode='r', encoding="utf-8-sig") as csv_file:
-        reader = csv.reader(csv_file, delimiter=',')
-        next(reader, None)
-        for row in reader:
-            processWorkoutType(row)
-            processTable(row,'songs')
-            processTable(row,'coaches')
-            processTable(row,'genres')
-            processTable(row,'intensities')
-
-def processTable(row, table):
-    # if no elems, skip
-    tablePk = data[table]['primary_key']
-    colTablePk = row[get(tablePk)]    
-    if colTablePk == '' : return
-    
-    # handle multiple elems in a row
-    tablePkList = [int(pk) for pk in colTablePk.split(',')]
-    numElems = len(tablePkList)
-    for i in range(numElems):
-        # skip if already encountered (from prev rows)
-        pk = tablePkList[i]
-        pkSet = data[table]['pk_set']
-        if pk in pkSet: continue
-        
-        # create entry, add elems to list (via headers), add pk to set  
-        tableElem = {}
-        headers = data[table]['headers']
-        for header in headers:
-            tableElem[header] = [item.strip() for item in row[get(header)].split(',')][i]
-        tableList = data[table]['items']
-        tableList.append(tableElem) 
-        pkSet.add(pk)
-
-def processWorkoutType(row):
-    processRow(row, data['workouts']['items'], data['workouts']['headers'])
-    workoutType = row[get('Workout Type')]
-    if(workoutType == 'classic'):
-        processRow(row, data['classic_workouts']['items'], data['classic_workouts']['headers'])
-    if(workoutType == 'boxing'):
-        processRow(row, data['boxing_workouts']['items'], data['boxing_workouts']['headers'])
-    if(workoutType == 'meditation'):
-        processRow(row, data['meditation_workouts']['items'], data['meditation_workouts']['headers'])
-    if(workoutType == 'stretch'):
-        processRow(row, data['stretch_workouts']['items'], data['stretch_workouts']['headers'])
-
-def processRow(row, list, headers):
-    data = {}
-    for header in headers:
-        data[header] = row[get(header)] if row[get(header)] != '' else 0
-    list.append(data)
-
-def createCSV(filename, headers, items):
-    with open(PATH + filename, "w", newline="", encoding="utf-8-sig") as f:
-        writer = csv.DictWriter(f, headers)
-        writer.writeheader()
-        writer.writerows(items)
-
-headers = getHeaders()
-runProcessor()
-for table in data.keys():
-    createCSV(data[table]['filename'], data[table]['headers'], data[table]['items'])
+main()
