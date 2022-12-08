5124dfee7f5c2a01f791e4172ae3272d4faddb1c
inocejoff <inocejoff@gmail.com>
2022-12-08 15:27:15 -0800

    load files and data processor changes

--- MySql/tableProcessor.py
+++ MySql/tableProcessor.py
@@ -21,7 +21,7 @@ INPUT_LOCATIONS_FILE = './Datasets/LOCATIONS_FLATTENED_11_29_2022.csv'
 PATH = "./Datasets/ProcessedFiles/"
 PROCESS_JSON = True
 PROCESS_CSV = False
-
+encode = 'utf-8-sig'
 def main():
     runProcessor()
     for table in createInstrxn.keys():
@@ -30,19 +30,23 @@ def main():
 
 
 def runLocationsProcessor():
-    with open(INPUT_LOCATIONS_FILE, mode='r', encoding="utf-8-sig") as csv_file:
+    with open(INPUT_LOCATIONS_FILE, mode='r', encoding=encode) as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
         next(reader, None)
         for row in reader:
             locationEntry = {}
             for i in range(len(row)):
                 colNames = createInstrxn['locations']['headers']
-                locationEntry[colNames[i]] = row[i]
+                if colNames[i] == 'Near the Ground':                    
+                    isTrue = row[i] == 'checked'
+                    locationEntry[colNames[i]] = True if isTrue else False
+                else:
+                    locationEntry[colNames[i]] = row[i]
             createInstrxn['locations']['items'].append(locationEntry)
 
 def getHeaders():
     headers = {}
-    with open(INPUT_WORKOUTS_FILE, mode='r', encoding="utf-8-sig") as csv_file:
+    with open(INPUT_WORKOUTS_FILE, mode='r', encoding=encode) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             for col in range(len(row)):
@@ -58,18 +62,18 @@ def get(val):
 def runProcessor():
     # process locations using locations data file
     runLocationsProcessor()
-    with open(INPUT_WORKOUTS_FILE, mode='r', encoding="utf-8-sig") as csv_file:
+    with open(INPUT_WORKOUTS_FILE, mode='r', encoding=encode) as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
         # skip header row
         next(reader, None)
         for row in reader:
-            processWorkoutType(row)
+            processWorkout(row)
             processTable(row,'songs')
             processTable(row,'coaches')
             processTable(row,'genres')
             processTable(row,'intensities')
             handleGenreList(row, 'genrelist')
-            handleSongMapLocationList(row, 'songmaps')
+            handleSongMapLocationList(row, 'songlocations')
 
 def handleGenreList(row, table):
     WorkoutId = row[get('SN ID')]
@@ -134,15 +138,9 @@ def processTable(row, table):
         tableList.append(tableElem) 
         pkSet.add(pk)
 
-def processWorkoutType(row):
+def processWorkout(row):
     # process all rows as a workout
     processRow(row, createInstrxn['workouts']['items'], createInstrxn['workouts']['headers'])
-    # process subcategorical workouts by type
-    workoutType = row[get('Workout Type')]
-    workoutTypes = ['classic', 'boxing', 'meditation', 'stretch']
-    for type in workoutTypes:
-        if workoutType == type:
-            processRow(row, createInstrxn[type]['items'], createInstrxn[type]['headers'])
 
 def processRow(row, list, headers):
     entry = {}
@@ -151,13 +149,13 @@ def processRow(row, list, headers):
     list.append(entry)
 
 def createCSV(filename, headers, items):
-    with open(PATH + 'CSV/' + filename, "w", newline="", encoding="utf-8-sig") as f:
+    with open(PATH + 'CSV/' + filename, "w", newline="", encoding=encode) as f:
         writer = csv.DictWriter(f, headers)
         writer.writeheader()
         writer.writerows(items)
 
 def createJSON(filename, items):
-    with open(PATH + 'JSON/' + filename + '.json', "w") as out:
+    with open(PATH + 'JSON/' + filename + '.json', "w", encoding='utf-8') as out:
         json.dump(items, out, indent=2)
 
 createInstrxn = {
@@ -166,39 +164,11 @@ createInstrxn = {
         'headers' : [
             'SN ID', 'Name', 'Description', 'Length', 
             'Date', 'Workout Type', 'Spotify Link', 
-            'SN ID (from Coach)', 'SN ID (from Intensity)' ],
+            'SN ID (from Coach)', 'SN ID (from Intensity)', 'Max Targets', 
+            'Max Dodges', 'Triangles', 'Max Knee Strikes', 'Max Score', 
+            'Points Per Minute', 'Targets Per Minute' ],
         'primary_key': 'SN ID',
         'filename' : 'WORKOUTS'
-    },
-    'classic': {
-        'items' : [],
-        'headers' : [
-            'SN ID', 'Max Targets', 'Triangles',
-            'Max Knee Strikes', 'Max Score', 
-            'Points Per Minute', 'Targets Per Minute' ],
-        'primary_key': 'SN ID', 
-        'filename' : 'CLASSIC_WORKOUTS'
-    },
-    'boxing': {
-        'items' : [],
-        'headers' : [
-            'SN ID', 'Max Targets', 'Max Dodges',
-            'Max Knee Strikes', 'Max Score', 
-            'Points Per Minute', 'Targets Per Minute' ],
-        'primary_key': 'SN ID', 
-        'filename' : 'BOXING_WORKOUTS'
-    },
-    'meditation': {
-        'items' : [],
-        'headers' : ['SN ID'],
-        'primary_key': 'SN ID', 
-        'filename' : 'MEDITATION_WORKOUTS'
-    },
-    'stretch': {
-        'items' : [],
-        'headers' : ['SN ID'],   
-        'primary_key': 'SN ID', 
-        'filename' : 'STRETCH_WORKOUTS'
     },
     'songs': {
         'pk_set': set(),
@@ -243,12 +213,12 @@ createInstrxn = {
             'SN ID', 'Near the Ground'], 
         'filename' : 'LOCATIONS'
     },
-    'songmaps': {
+    'songlocations': {
         'items' : [],
         'headers' : [
             'SN ID', 'SN ID (from Songs)',
             'SN ID (from Locations)'], 
-        'filename' : 'SONGMAPSLIST'
+        'filename' : 'SONGLOCATIONSLIST'
     },
     'genrelist': {
         'items' : [],
--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Build Dockerfile into image
 `docker image build -t docker-superunnatural-api .`
 Run the image into a container
 `docker run --rm -p 80:5000 docker-superunnatural-api`
+
+Loading the DB
+
+https://stackoverflow.com/questions/51789475/flask-sqlalchemy-load-database-with-records-by-running-python-script
+https://flask.palletsprojects.com/en/1.0.x/cli/#custom-commands
